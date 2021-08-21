a39209edfef0b0e6dad37f3bf3b3978505127c96
AnnaZen <87258850+AnnaZen@users.noreply.github.com>
2021-08-21 14:25:15 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//var a='hello world'
+//console.log(a)
+//console.log(2+2)
+//let apple='hello world'
+//console.log(apple)
+//if (true){
+//    console.log('i am true')
+//}
+//let condition=true
+//if(condition){
+ //   console.log('haha')
+//}
+//if (false){
+ //   console.log('anna')
+//}
+//else if(1){
+ //   console.log('anna zen')
+//}
+//else{
+  //  console.log('zen')
+//}
+let i=0
+while(i<10){
+  console.log('i am working')
+  i=i=1
+}
